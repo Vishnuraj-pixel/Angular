5d8d9ddd9483cc717a1158bc73956ed66cf45f10
Vishnuraj-pixel <vishnurajagopalan293@gmail.com>
2021-11-02 16:18:58 +0530

    First commit

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -2,19 +2,53 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl:  `
-  <h2>
-  Welcome {{name}}
-  </h2> 
-  <input [id]= "myId" type = "text" value = "Vishwas">
-  <input [isDisabled] = "isDisabled" id = "{{myId}}" type= "text" value = "Vishwas">  
-`, 
-  styles:[]
+  template: `
+    <h2>
+    Welcome {{name}}
+    </h2>
+    <p>{{name.length}}</p>
+    <p>{{siteUrl}}</p>
+    
+    <input id = "{{myId}}" type="text" [value] = "value">
+    <input [disabled] = isDisabled id = "{{myId}}" type="text" [value] = "value">
+    <input [class.text-danger] = "hasError" bind-id = "myId" type="text" [value] = "value">
+
+    <h2 [ngClass]= "messageClassess">Codevolution</h2>
+
+    <h2 [style.color] = "'orange'">Style Binding</h2>
+
+    <h2 [style.color] = "hasError ? 'red': 'green'">Style Binding 1</h2>
+
+    <h2 [style.color] = "highlightColor">Style Binding 2</h2>
+  `,
+  styles: [`
+   .text-success {
+      color: green; 
+   }
+   .text-danger {
+      color: #fcba03;
+   } 
+   .text-special {
+     font-style: italic;
+     font-family: 'Courier New', Courier, monospace;
+   }
+  `
+  ]
 })
 export class AppComponent {
+
   title = 'my-dream-app';
-  name = "testId";
-  isDiabled = false;
-
-
+  value = "Vishnu Rajagopalan"
+  name = "Vishnu Rajagopalan";
+  siteUrl = window.location.href;
+  myId = "testId";
+  isDisabled = false;
+  hasError = true;
+  isSpecial = true;
+  messageClassess = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
+  highlightColor = "Yellow";
 }
