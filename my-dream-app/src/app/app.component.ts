9fccffac969b5fd7390683a284ca28982f47e58b
Vishnuraj-pixel <vishnurajagopalan293@gmail.com>
2021-11-03 16:38:00 +0530

    first commit

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -28,6 +28,65 @@ import { Component } from '@angular/core';
     <button (click)= "greeting ='Welcome Vishwas'">Greet</button>
 
     {{greeting}}
+    <br>
+    <br>
+    sent message to the console
+    <input #myInput type="text">
+    <button (click)="logMessage(myInput.value)">Log</button>
+    <button (click)="logMessage(myInput)">Log</button>
+
+    <br>
+    <br>
+    <input [(ngModel)]= "name_1" type="text">
+    {{name_1}}{{name_1.length}}
+
+    <h2 *ngIf= "displayName else elseblock">Codevolution</h2>
+
+    <ng-template #elseblock>
+      <h2>Hidden Name</h2>
+    </ng-template>
+
+    <div *ngIf= "displayName then thenBlock; else elseBlock">Hello World</div>
+    <ng-template #thenBlock>
+    <h2>codevolution</h2>
+    </ng-template>
+
+    <ng-template #elseBlock>
+    <h2>Name is hidden</h2>
+    </ng-template>
+
+    <div [ngSwitch]= "color">
+      <div *ngSwitchCase= "'red'">You picked red color!</div>
+      <div *ngSwitchCase= "'green'">You picked green color!</div>
+      <div *ngSwitchCase= "'blue'">You picked blue color!</div>
+      <div *ngSwitchDefault>Pick again!</div>
+    </div>
+
+
+    <div *ngFor="let color of colors; index as i">
+     <h2>{{i}}. {{color}}</h2>
+
+    </div>
+
+    <div *ngFor="let color of colors; first as f">
+    <h2>{{f}}. {{color}}</h2>
+
+   </div>
+
+   <div *ngFor="let color of colors; last as l">
+    <h2>{{l}}. {{color}}</h2>
+
+   </div>
+
+   <div *ngFor="let color of colors; odd as o">
+    <h2>{{o}}. {{color}}</h2>
+
+   </div>
+
+   <div *ngFor="let color of colors; even as e">
+    <h2>{{e}}. {{color}}</h2>
+
+   </div>
   `,
   styles: [`
    .text-success {
@@ -45,6 +104,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  public name_1 = "";
+  colors = ['red', 'blue', 'green', 'saffron', 'yellow']
+  color = "yellow";
+  displayName = true;
   title = 'my-dream-app';
   value = "Vishnu Rajagopalan"
   name = "Vishnu Rajagopalan";
@@ -70,5 +133,10 @@ export class AppComponent {
     this.greeting = event.type;
   }
 
-  highlightColor = "Yellow";
+  highlightColor = "yellow";
+
+  logMessage(value: any) {
+    console.log(value); 
+  }
+
 }
